fe8608d47646abf4be7fdb025913e87c2ec41a29
NinaBhatia <ninabhatia@wustl.edu>
2019-08-29 15:39:57 -0500

    Nina and Emma Demo 1

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -12,7 +12,11 @@ public class Ordered {
 		int x = ap.nextInt("Value for x?");
 		int y = ap.nextInt("Value for y?");
 		int z = ap.nextInt("Value for z?");
+		boolean a = x>y;
+		boolean b = y>z;
+		boolean isTrue = (a && b) || (!a && !b);
 
+		System.out.println( x + ", " + y + ", " + z + " " + isTrue );
 	}
 
 }
